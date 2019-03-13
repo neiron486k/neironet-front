f2404c908e3fd10cd90f7cbe2c8c98a540410748
Alexey Astafev <efsneiron@gmail.com>
2019-03-13 16:31:29 +0300

    added text to jumbo

--- src/header/Header.js
+++ src/header/Header.js
@@ -5,10 +5,14 @@ import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import Typography from "@material-ui/core/es/Typography/Typography";
 import { Button } from "@material-ui/core";
-import PhoneIcon from "@material-ui/icons/Phone"
-import IconButton from "@material-ui/core/IconButton";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import background from "./images/background.jpg";
 import logo from "./images/logo.png";
+import DoneIcon from '@material-ui/icons/Done';
+import green from '@material-ui/core/colors/green'
 
 const styles = theme => ({
     root: {
@@ -28,6 +32,14 @@ const styles = theme => ({
     logo: {
         width: 50,
         height: 50
+    },
+    text: {
+        fontSize: '2em',
+        fontWeight: 'bold',
+        // color: "#004299"
+    },
+    icon: {
+        color: green['A700'],
     }
 });
 
@@ -55,6 +67,40 @@ const Header = ({ classes }) => (
                 </nav>
             </Toolbar>
         </AppBar>
+        <Toolbar>
+            <List>
+                <ListItem>
+                    <ListItemIcon className={classes.icon}>
+                        <DoneIcon />
+                    </ListItemIcon>
+                    <ListItemText
+                        classes={{
+                            primary: classes.text,
+                        }}
+                        primary="Developing"
+                        secondary={"php, python, lua, nodejs, js..."}
+                    />
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon className={classes.icon}>
+                        <DoneIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Administration" secondary={"unix, linux, windows, networks..."} classes={{ primary: classes.text }} />
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon className={classes.icon}>
+                        <DoneIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Consultation" secondary={"technologies, storing, etc..."} classes={{ primary: classes.text }} />
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon className={classes.icon}>
+                        <DoneIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Guarantee" secondary={"quality, times"} classes={{ primary: classes.text }} />
+                </ListItem>
+            </List>
+        </Toolbar>
     </CardMedia>
 );
 
