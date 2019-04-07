4fb24c69b5672408379439ff97191d4a908b1492
Alexey Astafev <efsneiron@gmail.com>
2019-04-07 15:42:20 +0300

    x

--- src/pages/landing/work/Work.js
+++ src/pages/landing/work/Work.js
@@ -65,7 +65,27 @@ const Work = ({ classes }) => {
         speed: 2000,
         autoplaySpeed: 6000,
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+        ],
     };
 
     return (
